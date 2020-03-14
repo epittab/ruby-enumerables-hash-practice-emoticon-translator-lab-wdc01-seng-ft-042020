e3fe37ff0ac8938fd643b3544bca7de9e1c89e7b
Enrique Pittaluga <ep7m@me.com>
2020-03-14 18:59:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,7 @@ def get_japanese_emoticon(filepath, emoticon)
   # code goes here
   hash = load_library(filepath)
   hash[:get_emoticon].each do |key, value|
-    if ()
+    if (emoticon == key)
     
   end
   return hash
